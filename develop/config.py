4cdd34bce3eddb37b85b8f9ffa174dd18471dab9
daheng <adamwang0705@gmail.com>
2017-10-09 18:53:58 -0400

    load original tweets p2

--- develop/config.py
+++ develop/config.py
@@ -41,3 +41,5 @@ TWEETS_PERIOD_DF_PKL = os.path.join(DATA_DIR, 'tweets-period.df.pkl')
 '''
 Misc
 '''
+# List of dates when Twitter internal server is unstable and tweets contain errors cannot be parsed
+ORIGINAL_TWEETS_ERROR_DATES_LST = ['2015-06-05', '2015-09-20', '2015-09-21', '2015-12-08', '2015-12-09', '2015-12-10', '2016-02-14', '2016-02-15', '2016-02-17', '2016-02-18', '2016-02-19']
