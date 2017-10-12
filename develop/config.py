3b1d11335c745bf40da56acd2bf8ad722b023187
daheng <adamwang0705@gmail.com>
2017-10-11 21:01:49 -0400

    add manually selected topics

--- develop/config.py
+++ develop/config.py
@@ -43,3 +43,29 @@ Misc
 '''
 # List of dates when Twitter internal server is unstable and tweets contain errors cannot be parsed
 ORIGINAL_TWEETS_ERROR_DATES_LST = ['2015-06-05', '2015-09-20', '2015-09-21', '2015-12-08', '2015-12-09', '2015-12-10', '2016-02-14', '2016-02-15', '2016-02-17', '2016-02-18', '2016-02-19']
+
+# Manully selected topics information
+MANUALLY_SELECTED_TOPICS_LST = [
+    {'category': 'politics', 'name': 'Hillary_Clinton_email_controversy', 'keywords_lst': [('email', 'e-mail'), ('Hillary', 'Clinton')]},
+    {'category': 'politics', 'name': 'Iran_nuclear_deal', 'keywords_lst': ['Iran', 'nuclear']},
+    {'category': 'politics', 'name': 'Jihadi_John_identity_reveled', 'keywords_lst': ['Jihadi John']},
+    {'category': 'social', 'name': 'Ferguson_unrest', 'keywords_lst': ['Ferguson']},
+    {'category': 'social', 'name': 'Hong_Kong_protests', 'keywords_lst': ['Hong Kong']},
+    {'category': 'social', 'name': 'Sony_cyberattack', 'keywords_lst': ['Sony']},
+    {'category': 'social', 'name': 'Bill_Cosby_sexual_assault_allegations ', 'keywords_lst': ['Bill Cosby']},
+    {'category': 'social', 'name': 'SpaceX_fails_rocket_landing ', 'keywords_lst': ['SpaceX']},
+    {'category': 'social', 'name': 'Brian_Williams_fake_war_story  ', 'keywords_lst': ['Brian Williams']},
+    {'category': 'entertainment', 'name': 'Oscar', 'keywords_lst': ['Oscar']},
+    {'category': 'entertainment', 'name': 'Super_Bowl', 'keywords_lst': ['Super Bowl']},
+    {'category': 'entertainment', 'name': 'Grammy', 'keywords_lst': ['Grammy']},
+    {'category': 'entertainment', 'name': 'Golden_Globe', 'keywords_lst': ['Golden Globe']},
+    {'category': 'entertainment', 'name': '500_million_Powerball', 'keywords_lst': ['Powerball']},
+    {'category': 'disaster', 'name': 'Ebola_virus', 'keywords_lst': ['Ebola']},
+    {'category': 'disaster', 'name': 'Indonesia_AirAsia_Flight_QZ8501', 'keywords_lst': [('AirAsia', '8501')]},
+    {'category': 'disaster', 'name': 'Paris_attacks', 'keywords_lst': ['Paris']},
+    {'category': 'disaster', 'name': 'Vanuatu_Cyclone_Pam', 'keywords_lst': ['Vanuatu', 'Cyclone']},
+    {'category': 'disaster', 'name': 'Malaysia_Airlines_Flight_MH370', 'keywords_lst': ['370']},
+    {'category': 'holiday', 'name': 'Thanksgiving', 'keywords_lst': ['Thanksgiving']},
+    {'category': 'holiday', 'name': 'Black_Friday_and_Cyber_Monday', 'keywords_lst': ['Black Friday', 'Cyber Monday']},
+    {'category': 'holiday', 'name': 'Christmas ', 'keywords_lst': ['Christmas ']},
+    {'category': 'holiday', 'name': 'New_Year ', 'keywords_lst': ['New Year ']}]
--- develop/utilities.py
+++ develop/utilities.py
@@ -22,9 +22,82 @@ STANDARD_DATETIME_TEMPLATE = "%Y-%m-%d %H:%M:%S"
 SIMPLE_DATE_TEMPLATE = "%Y-%m-%d"
 
 def parse_news_post_time(news_post_time_str):
+    """
+    Pasrse String in NEWS_POST_TIME_TEMPLATE format into String in STANDARD_DATETIME_TEMPLATE format
+    
+    PARAM news_post_time_str: String in NEWS_POST_TIME_TEMPLATE format
+    RETURN: parsed String in STANDARD_DATETIME_TEMPLATE format
+    """
     news_timestamp = time.strptime(news_post_time_str, NEWS_POST_TIME_TEMPLATE)
     return time.strftime(STANDARD_DATETIME_TEMPLATE, news_timestamp)
 
 def parse_tweet_post_time(tweet_post_time_str):
+    """
+    Pasrse String in TWEET_POST_TIME_TEMPLATE format into String in STANDARD_DATETIME_TEMPLATE format
+    
+    PARAM tweet_post_time_str: String in TWEET_POST_TIME_TEMPLATE format
+    RETURN: parsed String in STANDARD_DATETIME_TEMPLATE format
+    """
     tweet_timestamp = time.strptime(tweet_post_time_str, TWEET_POST_TIME_TEMPLATE)
     return time.strftime(STANDARD_DATETIME_TEMPLATE, tweet_timestamp)
+
+def news_title_match(news_title, topic_keywords_lst, verbose=False, case_sensitive=False):
+    """
+    Check if a news title contians keywords of a topic. All keywords in list should appear in the news title for it
+    to be qualified as a match. If a keyword is a tuple of items, any item appears in news title mark this news title
+    contains this tuple keyword.
+    
+    PARAM news_title: String of news title
+    PARAM topic_keywords_lst: list of keywords of the topic. Keyword can be a single String or tuple of items.
+    
+    RETURN: True or False if the news title contains topic keywords
+    """
+    keywords_match = True
+    if verbose: print('NEWS TITLE: {}'.format(news_title))
+    for keyword_ind, keyword in enumerate(topic_keywords_lst):
+        if isinstance(keyword, tuple):
+            if verbose: print('({}/{}) Tuple keyword: {}'.format(keyword_ind+1, len(topic_keywords_lst), keyword))
+            keyword_match = False
+            for item in keyword:
+                if verbose: print('\tTuple item: ({})'.format(item), end='; ')
+                
+                if case_sensitive:
+                    contains_item = item in news_title
+                else:
+                    contains_item = item.lower() in news_title.lower()
+                
+                if verbose: print('Contains?: {}'.format(contains_item))
+                keyword_match = keyword_match or contains_item
+            if verbose: print('\tContains Tuple?: {}'.format(keyword_match))
+            keywords_match = keywords_match and keyword_match
+        else:
+            if verbose: print('({}/{}) Keyword: {}'.format(keyword_ind+1, len(topic_keywords_lst), keyword))
+                
+            if case_sensitive:
+                keyword_match = keyword in news_title
+            else:
+                keyword_match = keyword.lower() in news_title.lower()
+            
+            if verbose: print('\tContains?: {}'.format(keyword_match))
+            keywords_match = keywords_match and keyword_match
+    if verbose: print('MATCH?: {}'.format(keywords_match), end='\n\n')
+    return keywords_match
+
+
+if '__main__' == __name__:
+    """
+    Tests on news_title_match() function
+    """
+    print('TESTS on news_title_match() function...')
+    test_topic_dict = {'cat': 'politics',
+                       'name': 'Hillary_Clinton_email_controversy',
+                       'keywords_lst': [('email', 'e-mail'), ('hillary', 'Clinton')]}
+    
+    test_news_titles = ["Hillary Clinton's Personal Email Use May Have Violated Federal Requirements ...", 
+                        "Clinton had no official State Dept. email address",
+                        "No entitlement reform: Hillary email scandal reveals core weakness",
+                        "House panel issues subpoenas for Clinton e-mails",
+                        "Ferguson cop e-mails insult Obamas"]
+    
+    for news_title in test_news_titles:
+        news_title_match(news_title, test_topic_dict['keywords_lst'], verbose=True)
